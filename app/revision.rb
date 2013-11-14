f0bd1b93848f072d2b065edf275432d2b8c9a059
yon <Lighthawky@gmail.com>
2013-11-14 10:53:16 +0200

    updated how revision works ;D

--- app/revision.rb
+++ app/revision.rb
@@ -7,16 +7,14 @@ env_name = EnvUtils.get_env_name(env)
 
 puts "Asking server for current revision of #{ColorUtil.colorize("#{project}:#{env}",:green)}"
 
-log_lines = `ey ssh "cat #{EnvUtils.ey_env}-deploy.log" -e #{env_name} | grep "Finished deploy at\\|git checkout"`
-revision = log_lines.scan(/git checkout -q '(\w+)'/).flatten.first
+status = `ey status -e #{env_name} | grep "Started at\\|Commit"`
+revision = status.match(/Commit:\s+(\w+)/)[1]
 if revision.nil? or revision.empty?
   CliUtils.error("No revision found")
 end
 puts "Revision: #{ColorUtil.colorize(revision,:yellow)}"
-GitUtils.create_tag(env, revision)
-puts "created tag"
 
-date = log_lines.scan(/Finished deploy at (.+)/).flatten.first
+date = status.match(/Started at:\s+(.+)/)[1]
 puts "Last Deployed at #{date}" unless date.nil? or date.empty?
 
 github_link = "https://github.com/#{EnvUtils.github_account}/#{project}/commit/#{revision}"
